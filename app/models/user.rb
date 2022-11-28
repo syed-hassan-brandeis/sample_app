1f72301dd76fdb9b5754d67fc53b980eeff58f36
syed-hassan-brandeis <syedhassan@brandeis.edu>
2022-11-28 20:31:53 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,10 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
   private
 
